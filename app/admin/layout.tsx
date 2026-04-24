27a73894ee3c904ca8c61138343828e53579702b
Ange <ishimwenzizaangell@gmail.com>
2026-04-24 16:04:18 +0200

    fix: made the first page admin dashboard

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -112,10 +112,12 @@ export default function AdminLayout({
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
               </div>
             </div>
-
+            <Link href={"/admin/notifications"}>
             <div className="flex h-10 w-10 items-center justify-center rounded-full bg-[var(--background)] text-[var(--muted)] transition hover:text-[var(--accent)] hover:bg-white hover:border border-[var(--border)] cursor-pointer">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>
             </div>
+            </Link>
+            <Link href={"/admin/profile"}>
             <div className="flex items-center gap-3">
               <div className="text-right">
                 <p className="text-sm font-bold text-[var(--foreground)] leading-none">System Admin</p>
@@ -123,6 +125,7 @@ export default function AdminLayout({
               </div>
               <div className="h-10 w-10 rounded-full bg-[var(--accent)] border-2 border-white shadow-md"></div>
             </div>
+            </Link>
             </div>
           </div>
         </header>
